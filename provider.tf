1ea356f9a78ae6f53ca94bb4f8fa16af10d66951
ratnesh2408 <75715592+ratnesh2408@users.noreply.github.com>
2020-12-12 10:41:30 +0530

    Update provider.tf

---
+++ provider.tf
@@ -0,0 +1,14 @@
+provider "aws" {
+  region = "${var.region}"
+  access_key = "AKIAXPH4GK5HGJ4Z64S4"
+  secret_key = "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"
+}
+
+/*
+terraform {
+  backend = "javahome-tf-1212"
+  key = "terraform.tfstate"
+  region = "us-east-1"
+ }
+}
+*/
